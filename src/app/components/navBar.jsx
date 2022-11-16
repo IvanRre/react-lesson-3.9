166a019421218e44f644f8b2d2a63b56bf82f2b5
Ivan Makarov <shtrohen@mail.ru>
2022-11-16 16:20:06 +0400

    start project

---
+++ src/app/components/navBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <ul>
+            <li><Link to="/">Main</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/users">Users</Link></li>
+        </ul>
+    );
+};
+
+export default NavBar;
